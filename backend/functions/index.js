b8e8fa6cac2980814fef00ef084e0dfa4cdb8a81
justdvnsh <justdvnsh2208@gmail.com>
2020-04-06 21:58:33 +0530

    added express apis

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const serviceAccount = require("../iosd-149b9-firebase-adminsdk-fs2xu-58d579b985.json")
+const firebase = require("firebase");
+const express = require("express");
+const app = express();
 
 var firebaseConfig = {
     apiKey: "AIzaSyCvYzIoJDegzFdPGcAHCBFtXshx-Ee7vWI",
@@ -9,37 +13,52 @@ var firebaseConfig = {
     storageBucket: "iosd-149b9.appspot.com",
     messagingSenderId: "97523930957",
     appId: "1:97523930957:web:6f89b452c11d2ce0c00904",
-    measurementId: "G-DZWFYHKZCL"
+    measurementId: "G-DZWFYHKZCL",
   };
 
+var adminConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://iosd-149b9.firebaseio.com"
+}
+
 // Initialize Firebase
-admin.initializeApp(firebaseConfig);
+admin.initializeApp(adminConfig);
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.getScreams = functions.https.onRequest((request, response) => {
-  admin.firestore().collection("screams").get()
+app.get("/screams", (request, response) => {
+  admin.firestore().collection("screams")
+  .orderBy("createdAt", "desc").get()
   .then((data) => {
     let screams = [];
-    data.forEach((doc) => screams.push(doc));
-    return screams;
+    data.forEach((doc) => screams.push({
+      screamId: doc.id,
+      userHandle: doc.data().userHandle,
+      body: doc.data().body,
+      createdAt: doc.data().createdAt
+    }));
+    return response.json(screams);
   }).catch(e => console.log(e))
 });
 
-exports.postScream = functions.https.onRequest((request, response) => {
+app.post("/screams", (request, response) => {
+  if (request.method !== "POST") {
+    return response.status(400).json({"error": "This is a POST method !"})
+  }
+  
   const newScream = {
     userHandle: request.body.userHandle,
     body: request.body.body,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   }
   admin.firestore().collection("screams").add(newScream)
   .then((data) => {
     return response.json({"message": `document ${data.id} created succesfully !`});
-  }).catch(e => console.log(e))
+  }).catch(e =>  {
+    console.log(e)
+    return response.status(500).json({error: "internal server error"});
+  })
 })
+
+exports.api = functions.https.onRequest(app);
